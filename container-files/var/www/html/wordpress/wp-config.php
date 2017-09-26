e08602ddc7b8183ebab32a3943397d5dff9bac54
Brandon Cone <bcone@esu10.org>
2017-09-26 13:26:33 -0500

    Some debugging help into wp-config.php

--- container-files/var/www/html/wordpress/wp-config.php
+++ container-files/var/www/html/wordpress/wp-config.php
@@ -17,7 +17,7 @@
  *
  * @package WordPress
  */
-
+print_r($_SERVER);
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', (($_SEVER['MYSQL_DATABASE'])?$_SEVER['MYSQL_DATABASE']:'wordpress'));
@@ -77,7 +77,7 @@ $table_prefix  = (($_SEVER['DATABASE_PREFIX'])? $_SERVER['DATABASE_PREFIX'] : 'w
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
