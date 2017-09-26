56316e9aa89a800293b92f51b747978b8541263c
Brandon Cone <bcone@esu10.org>
2017-09-26 08:49:52 -0500

    add vhost creation for specific wp install to install script

--- container-files/etc/cont-init.d/install-wordpress.sh
+++ container-files/etc/cont-init.d/install-wordpress.sh
@@ -1,4 +1,37 @@
 #!/usr/bin/with-contenv /bin/bash
+
+#Configurations specific to how we want to install and use
+#Wordpress need to be here and not in the Apache Container.
+#The question right now is if this runs prior to or following
+#the apache start command. If it is before, then we can write
+#our vhost file. If not, we need to figure out how to restart
+#the apache container after we create our vhost file.
+
+ #Write vhost file to the vhosts directory that will:
+ # 1) Update the Apache Document Root
+ # 2) Map the supplied URL to the Wordpress instance
+ # 3) Set up error logging
+
+#First, make sure the directory exists - in the event that this script
+# runs before the apache one.
+if [ -d /etc/httpd/conf/vhosts ]; then
+    echo "Vhosts directory exists"
+    #Do nothing
+else
+    echo "CREATING VHOSTS DIRECTORY FROM WORDPRESS RUN SCRIPT"
+    mkdir /etc/httpd/conf/vhosts
+fi
+ cat > /etc/httpd/conf/vhosts/vhost <<EOF
+ <VirtualHost *:80>
+    ServerAdmin brandon@esu10.org
+    DocumentRoot "/var/www/html/wordpress"
+    ServerName mydevsite.com
+    # ErrorLog /var/www/logs/ErrorLog
+    # AccessLog /var/www/logs/AccessLog
+ </VirtualHost>
+ 
+EOF
+
 cd /tmp
 if [ -d /var/www/html/wordpress/wp-content ]; then
 	echo "wp-content directory found, not overwriting";
